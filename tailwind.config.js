a20a92fa255b606da2240a05469dccd012b644ca
Alfa Sunny <alfasunny93@gmail.com>
2025-11-15 01:13:14 +0600

    Initial project setup done

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./*.{html,js}" // This looks for all .html and .js files in your root folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
